cd0a11265e1909e60d7c3d503111366f089ffe45
dxshulya <daraonelove@yandex.ru>
2022-07-11 14:44:41 +0300

    header todo

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Logo from "../../../img/logo.svg";
+
 import "./style.scss";
 
 function Header() {
@@ -7,7 +9,18 @@ function Header() {
     return (
         <header className="header">
             <div className="header__wrapper">
-
+                <div className="header__wrapper__logo">
+                    <img className="header__wrapper__logo__icon" src={Logo}/>
+                </div>
+                <div className="header__wrapper__menu">
+                    <nav className="header__wrapper__menu__list">
+                        <a href="" className="header__wrapper__menu__list__item">Sell online</a>
+                        <a href="" className="header__wrapper__menu__list__item">Fundraise</a>
+                        <a href="" className="header__wrapper__menu__list__item">Customize</a>
+                        <a href="" className="header__wrapper__menu__list__item">Explore</a>
+                    </nav>
+                    <button className="header__wrapper__menu__login">Log in</button>
+                </div>
             </div>
         </header>
     );
